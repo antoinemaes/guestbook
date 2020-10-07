73b969b0d36b9ef1f867d26229f82b58e4d0276f
Antoine Maes <antoinemaes@gmail.com>
2020-10-07 13:11:12 +0200

    add comment workflow

--- src/MessageHandler/CommentMessageHandler.php
+++ src/MessageHandler/CommentMessageHandler.php
@@ -4,7 +4,10 @@ namespace App\MessageHandler;
 
 
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Workflow\Registry;
 
 use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
@@ -16,30 +19,62 @@ class CommentMessageHandler implements MessageHandlerInterface
     private $spamChecker;
     private $entityManager;
     private $commentRepository;
+    private $bus;
+    private $workflowRegistry;
+    private $logger;
 
     public function __construct(
         EntityManagerInterface $entityManager, 
         SpamChecker $spamChecker, 
-        CommentRepository $commentRepository)
+        CommentRepository $commentRepository,
+        MessageBusInterface $bus,
+        Registry $workflowRegistry,
+        LoggerInterface $logger)
     {
         $this->entityManager = $entityManager;
         $this->spamChecker = $spamChecker;
         $this->commentRepository = $commentRepository;
+        $this->bus = $bus;
+        $this->workflowRegistry = $workflowRegistry;
+        $this->logger = $logger;
     }
 
     public function __invoke(CommentMessage $message)
+    
     {
+        
         $comment = $this->commentRepository->find($message->getId());
         if (!$comment) {
             return;
         }
+        $workflow = $this->workflowRegistry->get($comment);
 
-        if (2 === $this->spamChecker->getSpamScore($comment, $message->getContext())) {
-            $comment->setState('spam');
-        } else {
-            $comment->setState('published');
+        if ($workflow->can($comment, 'accept')) {
+            
+            $score = $this->spamChecker->getSpamScore($comment, $message->getContext());
+
+            switch($score) {
+                case 0: $transition = 'accept'; break;
+                case 1: $transition = 'might_be_spam'; break;
+                case 2: $transition = 'reject_spam'; break;
+            }
+
+            $workflow->apply($comment, $transition);
+            $this->entityManager->flush();
+            $this->bus->dispatch($message);
+
+        } elseif ($workflow->can($comment, 'publish') 
+            || $workflow->can($comment, 'publish_ham')) {
+        
+            $workflow->apply($comment, 
+                $workflow->can($comment, 'publish') ? 'publish' : 'publish_ham');
+            $this->entityManager->flush();
+
+        } elseif ($this->logger) {
+
+            $this->logger->debug('Dropping comment message', 
+                ['comment' => $comment->getId(), 'state' => $comment->getState()]);
+       
         }
-
-        $this->entityManager->flush();
     }
 }
